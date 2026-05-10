be847d1f3c91ae9a7b5b8b157ff95d79c4315bd4
GOVINSAGA <73192061+GOVINSAGA@users.noreply.github.com>
2026-05-10 17:59:34 +0530

    feat(backend): setup prisma service and global module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { MarketModule } from './market/market.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, PrismaModule, MarketModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
